14aad4bf7bb72a384f7146da1806c84493a979e0
kylian-plard <antoine.courant@hotmail.fr>
2017-03-09 15:46:15 +0100

    Ajout d'une ligne de Salut

--- plard_faria_forum/src/com/plard_faria_forum/controleur/HelloWorld.java
+++ plard_faria_forum/src/com/plard_faria_forum/controleur/HelloWorld.java
@@ -28,7 +28,7 @@ public class HelloWorld extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.getWriter().append("\nHello world !");
+		response.getWriter().append("\nHello world !\nHi Valentin ! :D");
 	}
 
 	/**
